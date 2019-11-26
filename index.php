712ed7ef25284dc2a37dc73517e17586f0ca1b74
Kesavan Shan <kns_8101@hotmail.com>
2019-11-25 20:49:56 -0500

    Update index.php

--- index.php
+++ index.php
@@ -123,7 +123,7 @@
     <!--Tensorflow-->
     <script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@1.0.0/dist/tf.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-vis@1.0.2/dist/tfjs-vis.umd.min.js"></script>
-	<!--<script type="module" src="src/main.js"></script>-->
+	<script type="module" src="src/main.js"></script>
 
 
     <!-- TODO: Add SDKs for Firebase products that you want to use
