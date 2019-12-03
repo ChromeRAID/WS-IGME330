14a7a8421ed20b251e135eda8420e9c7356a8ad0
Kesavan Shan <kns_8101@hotmail.com>
2019-12-03 18:58:28 -0500

    Psuedo-loading
    need to figure out how to detect when loading data

--- src/test.js
+++ src/test.js
@@ -4,6 +4,10 @@ let catResponse;
 
 let testURLDog = url + "dogs"
 let testURLCats = url + "cats"
+let status = {
+    isFinished: 1,
+    message: ""
+};
 //let url = "https://images.dog.ceo/breeds/hound-english/n02089973_48.jpg";
 //Create a new XHR object
 let xhr = new XMLHttpRequest();
@@ -14,8 +18,8 @@ xhr.onload = dataLoaded;
 xhr.onerror = dataError;
 
 //Open connection and set the request
-xhr.open("GET",testURLDog);
-xhr.setRequestHeader("Authorization","Client-ID 1226bd29241e849");
+xhr.open("GET", testURLDog);
+xhr.setRequestHeader("Authorization", "Client-ID 1226bd29241e849");
 xhr.send();
 
 
@@ -27,24 +31,23 @@ xhrC.onload = dataLoaded;
 xhrC.onerror = dataError;
 
 //Open connection and set the request
-xhrC.open("GET",testURLCats);
-xhrC.setRequestHeader("Authorization","Client-ID 1226bd29241e849");
+xhrC.open("GET", testURLCats);
+xhrC.setRequestHeader("Authorization", "Client-ID 1226bd29241e849");
 xhrC.send();
 
-function dataError(){
+function dataError() {
     console.log("ERROR LOADING DATA");
     console.log("ABORTING...");
-    console.log("ABORTED"); 
+    console.log("ABORTED");
 }
 
 let dataDict = {};
 
-function dataLoaded(e){
+function dataLoaded(e) {
     let label = e.target.responseURL.split("/")[6];
     let JSONObj = JSON.parse(e.target.responseText);
     let array = JSONObj.data;
-   
-    for(let i = 0; i<array.length; i++){
+    for (let i = 0; i < array.length; i++) {
         let imageUrl = array[i].link;
         let image = new Image();
         image.crossOrigin = "Anonymous";
@@ -52,107 +55,111 @@ function dataLoaded(e){
         image.onload = getData;
         image.class = label;
     }
-    
-    
 }
 
-function getData(e){
+function getData(e) {
+
+    //app.isLoading = false;
+    app.loadingMessage = "Loading images please wait...";
     let canvas = document.createElement("canvas");
     let image = e.path[0];
     canvas.width = 128;
     canvas.height = 128;
     let ctx = canvas.getContext("2d");
-    ctx.drawImage(image,0,0,128,128);
-    let data = ctx.getImageData(0,0,128,128);
-    if(dataDict[image.class] == undefined){
+    ctx.drawImage(image, 0, 0, 128, 128);
+    let data = ctx.getImageData(0, 0, 128, 128);
+    if (dataDict[image.class] == undefined) {
         dataDict[image.class] = [];
     }
     dataDict[image.class].push(data);
     console.log(dataDict);
+    //app.isFinished = true;
+    //app.loadingMessage = "";
 }
 
 let model;
-function createModel(){
+function createModel() {
     model = tf.sequential();
-    model.add(tf.layers.dense({units: 1, inputShape: [65536]}));
-    
-    model.compile({loss: 'meanSquaredError', optimizer: 'adam'});
-    
+    model.add(tf.layers.dense({ units: 1, inputShape: [65536] }));
+
+    model.compile({ loss: 'meanSquaredError', optimizer: 'adam' });
+
     let inputs = getInput(250);
-    
-    model.fit(inputs[0],inputs[1],{epochs:10}).then(()=>
-    {
+
+    model.fit(inputs[0], inputs[1], { epochs: 10 }).then(() => {
         predictTest();
     });
+    app.loadingMessage = "Training please wait...";
 }
 
 //returns a 2d tensor of image data
-function getInput(amountToGrab){
+function getInput(amountToGrab) {
     let inputArray = [];
     let labelArray = [];
-    let typesCount  = Object.keys(dataDict).length;
-    for(let i = 0; i<amountToGrab; i++){
-        let index = Math.floor(typesCount*Math.random());
+    let typesCount = Object.keys(dataDict).length;
+    for (let i = 0; i < amountToGrab; i++) {
+        let index = Math.floor(typesCount * Math.random());
         let type = Object.keys(dataDict)[index];
         labelArray.push(index);
         let fullArray = dataDict[type];
-        index = Math.floor(Math.random()*fullArray.length);
-        
+        index = Math.floor(Math.random() * fullArray.length);
+
         let data = fullArray[index];
-        for(let j = 0; j<data.data.length; j++){
-            inputArray.push(data.data[j]/255);
+        for (let j = 0; j < data.data.length; j++) {
+            inputArray.push(data.data[j] / 255);
         }
-        
-        
+
+
         //Image processing goes here
-       // dataInput = tf.image.resize(dataInput)
-        
-        
+        // dataInput = tf.image.resize(dataInput)
+
+
     }
-    let inputTensor = tf.tensor2d(inputArray,[amountToGrab,65536]);
-    let labelTensor = tf.tensor2d(labelArray,[amountToGrab,1]);
-    return [inputTensor,labelTensor];
+    let inputTensor = tf.tensor2d(inputArray, [amountToGrab, 65536]);
+    let labelTensor = tf.tensor2d(labelArray, [amountToGrab, 1]);
+    return [inputTensor, labelTensor];
 }
 
-function predict(imageData){
-    model.predict(tf.tensor2d(imageData,[65536,1])).print();
+function predict(imageData) {
+    model.predict(tf.tensor2d(imageData, [65536, 1])).print();
 }
 
-function predictTest(){
+function predictTest() {
     let inputArray = [];
-        let typesCount  = Object.keys(dataDict).length;
-        let index = Math.floor(typesCount*Math.random());
-        let type = Object.keys(dataDict)[index];
-        let fullArray = dataDict[type];
-        index = Math.floor(Math.random()*fullArray.length);
-        
-        let data = fullArray[index];
-        let dataInput = new Float32Array(65536);
-    
-        imagedata_to_image(data);
-    
-        for(let j = 0; j<data.data.length; j++){
-            inputArray.push(data.data[j]/255);
-        }
-        
-        let inputTensor = tf.tensor2d(inputArray,[1,65536]);
-    
-        let prediction = model.predict(inputTensor).asScalar();
-        
-        
-        const a = prediction;
-        var array = [];
-        array.push(a);
+    let typesCount = Object.keys(dataDict).length;
+    let index = Math.floor(typesCount * Math.random());
+    let type = Object.keys(dataDict)[index];
+    let fullArray = dataDict[type];
+    index = Math.floor(Math.random() * fullArray.length);
 
-        let values = array.map(t => t.dataSync()[0])
-        console.log(values);
-    
-        let val = values[0];
-        let predictType = Object.keys(dataDict)[Math.floor(val*typesCount)];
-        console.log(predictType);
-        app.guess = predictType;
-       return predictType;
-       
+    let data = fullArray[index];
+    let dataInput = new Float32Array(65536);
+
+    imagedata_to_image(data);
+
+    for (let j = 0; j < data.data.length; j++) {
+        inputArray.push(data.data[j] / 255);
+    }
+
+    let inputTensor = tf.tensor2d(inputArray, [1, 65536]);
+
+    let prediction = model.predict(inputTensor).asScalar();
+
+
+    const a = prediction;
+    var array = [];
+    array.push(a);
+
+    let values = array.map(t => t.dataSync()[0])
+    console.log(values);
+
+    let val = values[0];
+    let predictType = Object.keys(dataDict)[Math.floor(val * typesCount)];
+    console.log(predictType);
+    app.guess = `This is a ${predictType}. I'm ${values[0]}% sure. `;
+    app.loadingMessage = "";
+    return predictType;
+
 }
 
 function imagedata_to_image(imagedata) {
