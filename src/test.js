9fbf9f26c7ee2eeebdef1db50659fb8806d7ca63
Samuel Unferth (RIT Student) <sru4607@ad.rit.edu>
2019-12-03 15:11:29 -0500

    Worked on tensorflow

--- src/test.js
+++ src/test.js
@@ -70,3 +70,96 @@ function getData(e){
     dataDict[image.class].push(data);
     console.log(dataDict);
 }
+
+let model;
+function createModel(){
+    model = tf.sequential();
+    model.add(tf.layers.dense({units: 1, inputShape: [65536]}));
+    
+    model.compile({loss: 'meanSquaredError', optimizer: 'adam'});
+    
+    let inputs = getInput(250);
+    
+    model.fit(inputs[0],inputs[1],{epochs:10}).then(()=>
+    {
+        predictTest();
+    });
+}
+
+//returns a 2d tensor of image data
+function getInput(amountToGrab){
+    let inputArray = [];
+    let labelArray = [];
+    let typesCount  = Object.keys(dataDict).length;
+    for(let i = 0; i<amountToGrab; i++){
+        let index = Math.floor(typesCount*Math.random());
+        let type = Object.keys(dataDict)[index];
+        labelArray.push(index);
+        let fullArray = dataDict[type];
+        index = Math.floor(Math.random()*fullArray.length);
+        
+        let data = fullArray[index];
+        for(let j = 0; j<data.data.length; j++){
+            inputArray.push(data.data[j]/255);
+        }
+        
+        
+        //Image processing goes here
+       // dataInput = tf.image.resize(dataInput)
+        
+        
+    }
+    let inputTensor = tf.tensor2d(inputArray,[amountToGrab,65536]);
+    let labelTensor = tf.tensor2d(labelArray,[amountToGrab,1]);
+    return [inputTensor,labelTensor];
+}
+
+function predict(imageData){
+    model.predict(tf.tensor2d(imageData,[65536,1])).print();
+}
+
+function predictTest(){
+    let inputArray = [];
+        let typesCount  = Object.keys(dataDict).length;
+        let index = Math.floor(typesCount*Math.random());
+        let type = Object.keys(dataDict)[index];
+        let fullArray = dataDict[type];
+        index = Math.floor(Math.random()*fullArray.length);
+        
+        let data = fullArray[index];
+        let dataInput = new Float32Array(65536);
+    
+        imagedata_to_image(data);
+    
+        for(let j = 0; j<data.data.length; j++){
+            inputArray.push(data.data[j]/255);
+        }
+        
+        let inputTensor = tf.tensor2d(inputArray,[1,65536]);
+    
+        let prediction = model.predict(inputTensor).asScalar();
+        
+        
+        const a = prediction;
+        var array = [];
+        array.push(a);
+
+        let values = array.map(t => t.dataSync()[0])
+        console.log(values);
+    
+        let val = values[0];
+        let predictType = Object.keys(dataDict)[Math.floor(val*typesCount)];
+        console.log(predictType);
+        app.guess = predictType;
+       return predictType;
+       
+}
+
+function imagedata_to_image(imagedata) {
+    var canvas = document.querySelector('#mainCanvas');
+    var ctx = canvas.getContext('2d');
+    canvas.width = imagedata.width;
+    canvas.height = imagedata.height;
+    ctx.putImageData(imagedata, 0, 0);
+    app.image = canvas.toDataURL();
+}
